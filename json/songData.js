92a87e35e52f26c2f51a7393c853d809a8af8ffc
tyl330915 <66554645+tyl330915@users.noreply.github.com>
2021-08-31 10:07:51 -0400

    Update songData.js

--- json/songData.js
+++ json/songData.js
@@ -1153,7 +1153,7 @@ const songs = [{
         "lyricsURL": "https://genius.com/Pete-seeger-no-irish-need-apply-lyrics",
         "videoURL": "https://youtu.be/py363BW5_DE",
         "notesURL": "<a href=\"https://www.irishcentral.com/roots/history/clancy-brothers-no-irish-need-apply\" target=\"_blank\">History</a>",
-        "image": "https://irishamerica.com/wp-content/uploads/2016/12/FEATURE.No-Irish.jpg"
+        "image": "https://newsincerity.us/wp-content/uploads/2017/05/IrishNeedNotApply.jpg"
     }, {
         "_id": "60eeed5c0a0fac1c253cbf64",
         "title": "Solidarity Forever",
